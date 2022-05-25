9c87d9723afac83362bfb0a63a627dc48f01eab6
NovemLinguae <novemlinguae@gmail.com>
2022-05-25 15:33:32 -0700

    linting (end lines in semicolon, indent with tab)

--- CiteHighlighter/CiteHighlighter.js
+++ CiteHighlighter/CiteHighlighter.js
@@ -207,32 +207,32 @@ $(async function() {
 			}
 		}
 
-    function observeTooltips() {
-      new MutationObserver(function (mutations) {
-        var el = document.getElementsByClassName('rt-tooltip')[0]
-        if (el) {
-          for (let color in colors) {
-            if (typeof sources[color] === 'undefined') continue
+		function observeTooltips() {
+			new MutationObserver(function (mutations) {
+				var el = document.getElementsByClassName('rt-tooltip')[0];
+				if (el) {
+					for (let color in colors) {
+						if (typeof sources[color] === 'undefined') continue;
 
-            for (let source of sources[color]) {
-              total++
+						for (let source of sources[color]) {
+							total++;
 
-              if (wikicode.includes(source) || source === 'nih.gov' || source === 'twitter.com') {
-                i++
+							if (wikicode.includes(source) || source === 'nih.gov' || source === 'twitter.com') {
+								i++
 
-                if (source.includes('.') && !source.includes(' ')) {
-                  $(el).has(`a[href*="${source.toLowerCase()}"]`).addClass('cite-highlighter-' + color)
-                  $(el).has(`a[href*="${source.toLowerCase()}"]`).children().first().addClass('cite-highlighter-' + color)
-                }
-              }
-            }
-          }
-        }
-      }).observe(document.body, {
-        subtree: false,
-        childList: true,
-      })
-    }
+								if (source.includes('.') && !source.includes(' ')) {
+									$(el).has(`a[href*="${source.toLowerCase()}"]`).addClass('cite-highlighter-' + color);
+									$(el).has(`a[href*="${source.toLowerCase()}"]`).children().first().addClass('cite-highlighter-' + color);
+								}
+							}
+						}
+					}
+				}
+			}).observe(document.body, {
+				subtree: false,
+				childList: true,
+			});
+		}
 
 		// Be more aggressive with this list of words. Doesn't have to be the domain name. Can be anywhere in the URL. Example unreliableWord: blog.
 		for ( let word of unreliableWords ) {
@@ -245,7 +245,7 @@ $(async function() {
 			}
 		}
 
-		observeTooltips()
+		observeTooltips();
 
 		//console.log(`Added ${i} of ${total} CSS rules`);
 	});
