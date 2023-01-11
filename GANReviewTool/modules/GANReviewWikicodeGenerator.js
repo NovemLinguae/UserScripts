26633dd16eb36aa504ea165f0e47320a4a9a9a3c
NovemLinguae <novemlinguae@gmail.com>
2023-01-10 20:06:48 -0800

    fix regex bug

--- GANReviewTool/modules/GANReviewWikicodeGenerator.js
+++ GANReviewTool/modules/GANReviewWikicodeGenerator.js
@@ -740,7 +740,7 @@ export class GANReviewWikicodeGenerator {
 		let equalsSignsOnOneSide = /^(={2,5})/gm.exec(shortenedVersionInComboBox)[1];
 
 		// build a wider regex that includes equals, optional spaces next to the equals, optional [[File:]], and optional HTML comments
-		let regex = new RegExp(`^${equalsSignsOnOneSide}\\s*(?:\\[\\[File:[^\\]]*\\]\\]\\s*)?${this.regExEscape(needle)}\\s*\s*(?:<!--[^\\-]*-->)?\\s*${equalsSignsOnOneSide}$`, 'gm');
+		let regex = new RegExp(`^${equalsSignsOnOneSide}\\s*(?:\\[\\[File:[^\\]]*\\]\\]\\s*)?${this.regExEscape(needle)}\\s*(?:<!--[^\\-]*-->)?\\s*${equalsSignsOnOneSide}$`, 'gm');
 		let result = regex.exec(wikicode);
 
 		let resultNotFound = result === null;
