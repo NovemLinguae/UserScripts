eb35687424ba09d7ab6bfb40d730d1ff0066ec7e
Eejit <76887639+Eejit43@users.noreply.github.com>
2023-02-19 14:28:27 -0500

    Update link location, notification method, and namespace check (#152)
    * Update link location and notification method
    
    * resolve errors
    
    * support placing in both tools menu and more menu
    
    * indent
    
    * comment
    
    ---------
    
    Co-authored-by: NovemLinguae <novemlinguae@gmail.com>

--- DraftCleaner/main.js
+++ DraftCleaner/main.js
@@ -37,6 +37,10 @@
 	- if article has headings but no lead, remove first heading
 	- replace unicode bullets with asterisks
 
+Add one of the following to your User:yourName/common.js (at the top) to change the position where DraftCleaner puts its link:
+    window.draftCleanerPutInToolsMenu = true;
+	window.draftCleanerPutInMoreMenu = true;
+
 This page was assembled from 3 files using my publish.php script. I have an offline test suite with around 100 unit tests for the DraftCleaner and StringFilter classes.
 */
 
@@ -93,18 +97,6 @@ function getArticleName() {
 	return mw.config.get('wgPageName');
 }
 
-function showMessage(messageText) {
-	$('#DraftCleaner').hide();
-	$('#DraftCleanerNoClick').empty();
-	$('#DraftCleanerNoClick').prepend(messageText);
-	$('#DraftCleanerNoClick').show();
-}
-
-function showClickableButton() {
-	$('#DraftCleanerNoClick').hide();
-	$('#DraftCleaner').show();
-}
-
 /** refresh AND clear cache */
 function hardRefresh() {
 	// window.location.reload(true) is deprecated. use this instead
@@ -119,51 +111,51 @@ if ( action != 'view' ) return;
 let isDiff = mw.config.get('wgDiffNewId');
 if ( isDiff ) return;
 
-// Only run in mainspace, draftspace, and sandboxes
+// Don't run in virtual namespaces
+if (mw.config.get('wgNamespaceNumber') < 0) return;
+
+let menuID = 'p-navigation';
+// @ts-ignore
+if ( window.draftCleanerPutInToolsMenu ) {
+	menuID = 'p-tb';
+// @ts-ignore
+} else if ( window.draftCleanerPutInMoreMenu ) {
+	menuID = 'p-cactions';
+}
+
 let titleWithNamespaceAndUnderscores = getArticleName();
 let namespaceNumber = mw.config.get('wgNamespaceNumber');
-let sandbox = titleWithNamespaceAndUnderscores.match(/sandbox/i);
-//if ( ! [0, 118].includes(namespaceNumber) && ! sandbox ) return;
 
-// @ts-ignore
-let menuID = window.draftCleanerPutInToolsMenu ? 'p-tb' : 'p-navigation';
+let running = false;
 
-// Add DraftCleaner to left sidebar
-// Using two <li>s. One of the two is kept hidden at all times. This avoids having to delete #DraftCleanerLink, which would also delete the event listener.
-$(`#${menuID} ul`).append(`
-	<li id="DraftCleaner">
-		<a id="DraftCleanerLink">Run DraftCleaner</a>
-	</li>
+// Add DraftCleaner to the toolbar
+mw.loader.using(['mediawiki.util'], function () {
+	mw.util.addPortletLink(menuID, '#', 'Run DraftCleaner', 'DraftCleanerLink');
+	$('#DraftCleanerLink').on('click', async function() {
+		// prevent running the script while script is already in progress
+		if (running) return;
+		running = true;
+
+		// notify of script starting
+		mw.notify('Parsing page content...');
 	
-	<li id="DraftCleanerNoClick" style="display:none">
-		
-	</li>
-`);
+		// get page wikicode
+		let titleWithNamespaceAndSpaces = titleWithNamespaceAndUnderscores.replace(/_/g, ' ');
+		let originalWikicode = await getWikicode(titleWithNamespaceAndUnderscores);
+		let wikicode = originalWikicode;
 
-$('#DraftCleanerLink').on('click', async function() {
-	// prevent running the script while script is already in progress
-	showMessage('Editing. Please wait.');
-	
-	// get page wikicode
-	let titleWithNamespaceAndSpaces = titleWithNamespaceAndUnderscores.replace(/_/g, ' ');
-	let originalWikicode = await getWikicode(titleWithNamespaceAndUnderscores);
-	let wikicode = originalWikicode;
+		let dc = new DraftCleaner();
+		wikicode = dc.cleanDraft(wikicode, namespaceNumber, titleWithNamespaceAndSpaces);
 
-	let dc = new DraftCleaner();
-	wikicode = dc.cleanDraft(wikicode, namespaceNumber, titleWithNamespaceAndSpaces);
-
-	// if changes to be made
-	if ( wikicode != originalWikicode ) {
-		let summary = 'clean up ([[User:Novem Linguae/Scripts/DraftCleaner.js|DraftCleaner]])';
-		// editPage(titleWithNamespaceAndUnderscores, wikicode);
-		// hardRefresh();
-		await goToShowChangesScreen(titleWithNamespaceAndUnderscores, wikicode, summary);
-	// else display "no changes needed", then reset
-	} else {
-		showMessage('No changes needed.');
-		
-		setTimeout(function (){
-			showClickableButton();
-		}, 2000);
-	}
+		// if changes to be made
+		if ( wikicode != originalWikicode ) {
+			let summary = 'clean up ([[User:Novem Linguae/Scripts/DraftCleaner.js|DraftCleaner]])';
+			// editPage(titleWithNamespaceAndUnderscores, wikicode);
+			// hardRefresh();
+			await goToShowChangesScreen(titleWithNamespaceAndUnderscores, wikicode, summary);
+		// else display "no changes needed", then reset
+		} else {
+			mw.notify('No changes needed!');
+		}
+	});
 });
