a8ba90f434f92135ad95b348f22abb3181624544
NovemLinguae <novemlinguae@gmail.com>
2023-04-06 14:24:09 -0700

    improve CiteHighlighter update tool

--- CiteHighlighter/NPPSG to array tool/index.php
+++ CiteHighlighter/NPPSG to array tool/index.php
@@ -25,13 +25,22 @@ $inputTextArea = getWikitextFromEnWiki('Wikipedia:New page patrol source guide')
 			Cite Highlighter - NPPSG and MySources to array tool
 		</h1>
 		<p>
+			Automatically loads the contents of <a href="https://en.wikipedia.org/wiki/Wikipedia:New_page_patrol_source_guide" target="_blank">Wikipedia:New page patrol source guide</a> and <a href="https://en.wikipedia.org/wiki/User:Novem_Linguae/Scripts/CiteHighlighter/AllSourcesExceptNPPSG" target="_blank">User:Novem Linguae/Scripts/CiteHighlighter/AllSourcesExceptNPPSG</a>, then parses it into JSON below. There are also some hard-coded domains and changes in this tool's Javascript logic.
+		</p>
+		<p style="display:none;">
 			<textarea id="input"><?= $inputTextArea; ?></textarea>
 		</p>
-		<p>
+		<p style="display:none;">
 			<button id="execute">Execute</button>
 		</p>
 		<p>
 			<textarea id="output" style="white-space: unset;"></textarea>
+		</p>
+		<p>
+			<a href="https://en.wikipedia.org/w/index.php?title=User:Novem_Linguae/Scripts/CiteHighlighter/SourcesJSON.js&action=edit" target="_blank">Click here</a> to open the page where this JSON needs to be copy pasted to on wiki.
+		</p>
+		<p>
+			<a href="https://en.wikipedia.org/wiki/User:Novem_Linguae/Scripts/CiteHighlighter/testcases" target="_blank">Click here</a> to load the testcases page.
 		</p>
 	</body>
 </html>
