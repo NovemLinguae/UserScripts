15239a6d902c7d79c8b3e9e81cee09588b85ab6c
NovemLinguae <novemlinguae@gmail.com>
2023-12-22 23:51:51 -0800

    refactor from functions to class

--- DetectPromo.js
+++ DetectPromo.js
@@ -5,66 +5,9 @@
 - Displays an orange bar at the top of the article, listing the detected keywords.
 */
 
-$(async function() {
-	async function getWikicode(title) {
-		if ( ! mw.config.get('wgCurRevisionId') ) {
-			return ''; // if page is deleted, return blank
-		}
-		var wikicode = '';
-		title = encodeURIComponent(title);
-		await $.ajax({
-			url: 'https://en.wikipedia.org/w/api.php?action=parse&page=' + title + '&prop=wikitext&formatversion=2&format=json',
-			success: function (result) {
-				wikicode = result['parse']['wikitext'];
-			},
-			dataType: "json",
-			async: false
-		});
-		return wikicode;
-	}
-
-	function eliminateDuplicates(array) {
-		return [...new Set(array)];
-	}
-
-	/** returns the pagename, including the namespace name, but with spaces replaced by underscores */
-	function getArticleName() {
-		return mw.config.get('wgPageName');
-	}
-
-	function empty(arr) {
-		return !!(arr === undefined || arr.length == 0);
-	}
-
-	function escapeRegEx(string) {
-		return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'); // $& means the whole matched string
-	}
-
-	// don't run when not viewing articles
-	let action = mw.config.get('wgAction');
-	if ( action != 'view' ) {
-		return;
-	}
-
-	// don't run when viewing diffs
-	let isDiff = mw.config.get('wgDiffNewId');
-	if ( isDiff ) {
-		return;
-	}
-
-	let isDeletedPage = ! mw.config.get('wgCurRevisionId') ;
-	if ( isDeletedPage ) {
-		return;
-	}
-
-	// Only run in mainspace and draftspace
-	let namespace = mw.config.get('wgNamespaceNumber');
-	let title = getArticleName();
-	if ( ! [0, 118].includes(namespace) && title != 'User:Novem_Linguae/sandbox' ) {
-		return;
-	}
-
-	let wordString = `
+class DetectPromo {
+	/** @member {string} */
+	wordString = `
 
 // An impressive amount of promo in this draft: https://en.wikipedia.org/w/index.php?title=Draft:Imre_Van_opstal&oldid=1060259849
 
@@ -198,51 +141,153 @@ indelible
 
 	`;
 
-	wordString = wordString.replace(/^\/\/.*$/gm, ''); // replace comment lines with blank lines. using this approach fixes a bug involving // and comma on the same line
-	let wordArray = wordString.replace(/, /g, "\n")
-		.trim()
-		.split("\n")
-		.map(v => v.trim())
-		.filter(v => v != '')
-		.filter(v => ! v.startsWith('//'));
-	wordArray = eliminateDuplicates(wordArray);
-
-	// convert from 1 level array with just text, to 2 level array with text and regex
-	let wordObject = [];
-	for ( let key in wordArray ) {
-		wordObject.push({
-			'text': wordArray[key],
-			'regex': escapeRegEx(wordArray[key])
-		});
+	/**
+	 * @param {object} mw
+	 * @param {jQuery} $
+	 */
+	constructor(mw, $) {
+		this.mw = mw;
+		this.$ = $;
 	}
 
-	let wikicode = await getWikicode(title);
+	async execute() {
+		if ( ! this.shouldRunOnThisPage() ) {
+			return;
+		}
+		let wordObject = this.getWordObject();
+		let title = this.mw.config.get('wgPageName');
+		let wikicode = await this.getWikicode(title);
+		wikicode = this.cleanWikicode(wikicode);
 
-	// eliminate [[ ]], so that phrases with wikilink syntax in the middle don't mess up our search
-	wikicode = wikicode.replace(/\[\[/g, '')
-		.replace(/\]\]/g, '');
+		let searchResults = [];
+		for ( let word of wordObject ) {
+			// can't use \b here because \)\b doesn't work correctly. using lookarounds instead
+			let regEx = new RegExp('(?<!\\w)' + word['regex'] + '(?!\\w)', "i");
+			if ( wikicode.match(regEx) ) {
+				searchResults.push(word['text']);
+			}
+		}
 
-	let searchResults = [];
-	for ( let word of wordObject ) {
-		// can't use \b here because \)\b doesn't work correctly. using lookarounds instead
-		let regEx = new RegExp('(?<!\\w)' + word['regex'] + '(?!\\w)', "i");
-		if ( wikicode.match(regEx) ) {
-			searchResults.push(word['text']);
+		let MAX_DISPLAYED_RESULTS = 20;
+		if ( searchResults.length > MAX_DISPLAYED_RESULTS ) {
+			searchResults = searchResults.slice(0, MAX_DISPLAYED_RESULTS);
+			searchResults.push('...... and more.');
+		}
+
+		if ( ! this.empty(searchResults) ) {
+			let html = searchResults.join(', ');
+			html = '<div id="DetectPromo" style="background-color: orange"><span style="font-weight: bold;">Promotional words:</span> ' + html + '</div>';
+
+			this.$('#contentSub').before(html);
 		}
 	}
 
-	let MAX_DISPLAYED_RESULTS = 20;
-	if ( searchResults.length > MAX_DISPLAYED_RESULTS ) {
-		searchResults = searchResults.slice(0, MAX_DISPLAYED_RESULTS);
-		searchResults.push('...... and more.');
+	/**
+	 * @param {string} wikicode
+	 * @returns {string} wikicode
+	 */
+	cleanWikicode(wikicode) {
+		// eliminate [[ ]], so that phrases with wikilink syntax in the middle don't mess up our search
+		wikicode = wikicode.replace(/\[\[/g, '')
+			.replace(/\]\]/g, '');
+		return wikicode;
 	}
 
-	if ( ! empty(searchResults) ) {
-		let html = searchResults.join(', ');
-		html = '<div id="DetectPromo" style="background-color: orange"><span style="font-weight: bold;">Promotional words:</span> ' + html + '</div>';
+	/**
+	 * @returns {Array} wordObject - An array of objects in the format `[ {text: 'state of the art', regex: 'state of the art'}, {text: 'world class', regex: 'world class'} ]`
+	 */
+	getWordObject() {
+		this.wordString = this.wordString.replace(/^\/\/.*$/gm, ''); // replace comment lines with blank lines. using this approach fixes a bug involving // and comma on the same line
+		let wordArray = this.wordString.replace(/, /g, "\n")
+			.trim()
+			.split("\n")
+			.map(v => v.trim())
+			.filter(v => v != '')
+			.filter(v => ! v.startsWith('//'));
+		wordArray = this.eliminateDuplicates(wordArray);
 
-		$('#contentSub').before(html);
+		// convert from 1 level array with just text, to 2 level array with text and regex
+		let wordObject = [];
+		for ( let key in wordArray ) {
+			wordObject.push({
+				'text': wordArray[key],
+				'regex': this.escapeRegEx(wordArray[key])
+			});
+		}
+
+		return wordObject;
 	}
+
+	/**
+	 * @returns {boolean}
+	 */
+	shouldRunOnThisPage() {
+		// don't run when not viewing articles
+		let action = this.mw.config.get('wgAction');
+		if ( action != 'view' ) {
+			return false;
+		}
+
+		// don't run when viewing diffs
+		let isDiff = this.mw.config.get('wgDiffNewId');
+		if ( isDiff ) {
+			return false;
+		}
+
+		let isDeletedPage = ! this.mw.config.get('wgCurRevisionId') ;
+		if ( isDeletedPage ) {
+			return false;
+		}
+
+		// Only run in mainspace and draftspace
+		let namespace = this.mw.config.get('wgNamespaceNumber');
+		let title = this.mw.config.get('wgPageName');
+		if ( ! [0, 118].includes(namespace) && title != 'User:Novem_Linguae/sandbox' ) {
+			return false;
+		}
+
+		return true;
+	}
+
+	/**
+	 * @param {string} title
+	 * @returns {string} wikicode
+	 */
+	async getWikicode(title) {
+		let pageIsDeleted = ! this.mw.config.get('wgCurRevisionId');
+		if ( pageIsDeleted ) {
+			return '';
+		}
+
+		let api = new this.mw.Api();
+		let response = await api.get( {
+			action: 'parse',
+			page: title,
+			prop: 'wikitext',
+			formatversion: '2',
+			format: 'json'
+		} );
+		return response.parse.wikitext;
+	}
+
+	eliminateDuplicates(array) {
+		return [...new Set(array)];
+	}
+
+	empty(arr) {
+		return !!(arr === undefined || arr.length == 0);
+	}
+
+	escapeRegEx(string) {
+		return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'); // $& means the whole matched string
+	}
+}
+
+$(async function() {
+	await mw.loader.using(['mediawiki.api'], async () => {
+		let detectPromo = new DetectPromo(mw, $);
+		await detectPromo.execute();
+	});
 });
 
 // </nowiki>
