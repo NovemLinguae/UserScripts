f7f06f8452031dc78f3eb1ed369c48afae6a038a
NovemLinguae <novemlinguae@gmail.com>
2024-04-25 06:08:47 -0700

    fix linter errors

--- ReviewStatus.js
+++ ReviewStatus.js
@@ -11,66 +11,66 @@
 
 class ReviewStatus {
 	async execute() {
-		if ( ! this.shouldRunOnThisPage() ) {
+		if ( !this.shouldRunOnThisPage() ) {
 			return;
 		}
 
-		let pageID = mw.config.get('wgArticleId');
-		let boolIsReviewed = await this.isReviewed(pageID);
+		const pageID = mw.config.get( 'wgArticleId' );
+		const boolIsReviewed = await this.isReviewed( pageID );
 		let htmlToInsert = '';
 
 		// modules/ext.pageTriage.views.toolbar/images/icons/
 
 		if ( boolIsReviewed ) {
-			htmlToInsert = ` <img src="https://en.wikipedia.org/w/extensions/PageTriage/modules/ext.pageTriage.toolbar/images/pageInfo/icon_reviewed.png" title="Reviewed" />`;
+			htmlToInsert = ' <img src="https://en.wikipedia.org/w/extensions/PageTriage/modules/ext.pageTriage.toolbar/images/pageInfo/icon_reviewed.png" title="Reviewed" />';
 		} else {
-			htmlToInsert = ` <img src="https://en.wikipedia.org/w/extensions/PageTriage/modules/ext.pageTriage.toolbar/images/pageInfo/icon_not_reviewed.png" title="Not reviewed" />`;
+			htmlToInsert = ' <img src="https://en.wikipedia.org/w/extensions/PageTriage/modules/ext.pageTriage.toolbar/images/pageInfo/icon_not_reviewed.png" title="Not reviewed" />';
 		}
 
 		if ( this.pageHasSections() ) {
-			$(`#firstHeading .mw-editsection`).before(htmlToInsert);
+			$( '#firstHeading .mw-editsection' ).before( htmlToInsert );
 		} else {
-			$(`#firstHeading`).append(htmlToInsert);
+			$( '#firstHeading' ).append( htmlToInsert );
 		}
 	}
 
 	/**
 	 * @param {number} pageID The page ID number. A positive number with no commas.
 	 */
-	async isReviewed(pageID) {
-		let api = new mw.Api();
-		let response = await api.get( {
+	async isReviewed( pageID ) {
+		const api = new mw.Api();
+		const response = await api.get( {
 			action: 'query',
 			format: 'json',
 			formatversion: '2',
 			prop: 'isreviewed',
-			pageids: pageID,
+			pageids: pageID
 		} );
-		return response.query.pages[0].isreviewed;
+		return response.query.pages[ 0 ].isreviewed;
 	}
 
 	shouldRunOnThisPage() {
 		// don't run when not viewing articles
-		let action = mw.config.get('wgAction');
+		const action = mw.config.get( 'wgAction' );
 		if ( action !== 'view' ) {
 			return false;
 		}
 
 		// don't run when viewing diffs
-		let isDiff = mw.config.get('wgDiffNewId');
+		const isDiff = mw.config.get( 'wgDiffNewId' );
 		if ( isDiff ) {
 			return false;
 		}
 
-		let isDeletedPage = ( ! mw.config.get('wgCurRevisionId') );
+		const isDeletedPage = ( !mw.config.get( 'wgCurRevisionId' ) );
 		if ( isDeletedPage ) {
 			return false;
 		}
 
 		// Only run in mainspace
-		let namespace = mw.config.get('wgNamespaceNumber');
-		let isMainspaceOrDraftspace = ( [0].includes(namespace) );
-		if ( ! isMainspaceOrDraftspace ) {
+		const namespace = mw.config.get( 'wgNamespaceNumber' );
+		const isMainspaceOrDraftspace = ( [ 0 ].includes( namespace ) );
+		if ( !isMainspaceOrDraftspace ) {
 			return false;
 		}
 
@@ -78,15 +78,14 @@ class ReviewStatus {
 	}
 
 	pageHasSections() {
-		return $(`#firstHeading .mw-editsection`).length;
+		return $( '#firstHeading .mw-editsection' ).length;
 	}
 }
 
-$(async function() {
-	await mw.loader.using(['mediawiki.api'], async () => {
-		let rs = new ReviewStatus();
-		await rs.execute();
-	});
-});
+$( async function () {
+	await mw.loader.using( [ 'mediawiki.api' ], async function () {
+		await ( new ReviewStatus() ).execute();
+	} );
+} );
 
 // </nowiki>
