1c03904d3f6a1ff9bc110250466a4773f93ca714
NovemLinguae <novemlinguae@gmail.com>
2024-07-02 10:37:13 -0700

    MarkBlocked: move down variable declaration

--- Gadget-markblocked.js
+++ Gadget-markblocked.js
@@ -174,19 +174,19 @@ Configuration variables:
 
 		const ipv6Regex = /^((?=.*::)(?!.*::.+::)(::)?([\dA-F]{1,4}:(:|\b)|){5}|([\dA-F]{1,4}:){6})((([\dA-F]{1,4}((?!\3)::|:\b|$))|(?!\2\3)){2}|(((2[0-4]|1\d|[1-9])?\d|25[0-5])\.?\b){4})$/i;
 
-		let user, url, pageTitle;
 		$contentLinks.each( ( i, link ) => {
 			if ( $( link ).is( '.mw-changeslist-date, .ext-discussiontools-init-timestamplink, .mw-history-undo > a, .mw-rollback-link > a' ) ) {
 				return;
 			}
 
-			url = $( link ).attr( 'href' );
+			const url = $( link ).attr( 'href' );
 			if ( !url ) {
 				return;
 			}
 
 			const articleMatch = articleRegex.exec( url ),
 				scriptMatch = scriptRegex.exec( url );
+			let pageTitle;
 			if ( articleMatch ) {
 				pageTitle = articleMatch[ 1 ];
 			} else if ( scriptMatch ) {
@@ -196,7 +196,7 @@ Configuration variables:
 			}
 			pageTitle = decodeURIComponent( pageTitle ).replace( /_/g, ' ' );
 
-			user = userTitleRegex.exec( pageTitle );
+			let user = userTitleRegex.exec( pageTitle );
 			if ( !user ) {
 				return;
 			}
