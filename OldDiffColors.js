7eae6b244df416d7b81ec29441c4cb099b6fafac
NovemLinguae <novemlinguae@gmail.com>
2024-07-06 18:08:47 -0700

    OldDiffColors: handle recent DiscussionTools color changes

--- OldDiffColors.js
+++ OldDiffColors.js
@@ -1,4 +1,6 @@
 // Changes diff colors from yellow for removal and purple for addition, back to yellow for removal and blue for addition.
+// Fixes diff "black dot" bug when interacting with some user scripts.
+// Changes DiscussionTools published comment color from teal to light blue.
 
 // Forked from code by https://en.wikipedia.org/wiki/User:Hgzh, CC BY-SA 4.0, https://en.wikipedia.org/wiki/Wikipedia:Village_pump_(technical)/Archive_213#Heads-up%3A_Diff_colour
 
@@ -23,6 +25,13 @@ mw.util.addCSS( `
 a.mw-diff-movedpara-left,
 a.mw-diff-movedpara-right {
 	 color: transparent;
+}
+
+.ext-discussiontools-init-targetcomment {
+	background-color: #eaf3ff;
+}
+.ext-discussiontools-init-publishedcomment {
+	background-color: #eaf3ff; /* not #d5fdf4 */
 }
 
 ` );
