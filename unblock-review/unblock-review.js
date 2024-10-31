2d7685af310433e347d55f8cdb178dc6d064a943
NovemLinguae <novemlinguae@gmail.com>
2024-10-30 20:16:15 -0700

    unblock-review: autofix linter errors

--- unblock-review/unblock-review.js
+++ unblock-review/unblock-review.js
@@ -1,197 +1,200 @@
 // Fork of https://en.wikipedia.org/w/index.php?title=User:Enterprisey/unblock-review.js&oldid=1073685522
-// Thanks [[User:Enterprisey]]!
 
 // <nowiki>
 ( function () {
-    var UNBLOCK_REQ_COLOR = "rgb(235, 244, 255)";
-    var SIGNATURE = "~~" + "~~";
-    var DECLINE_REASON_HERE = "{" + "{subst:Decline reason here}}"; // broken up to prevent processing
-    var ADVERT = " ([[User:Enterprisey/unblock-review|unblock-review]])";
+	const UNBLOCK_REQ_COLOR = 'rgb(235, 244, 255)';
+	const SIGNATURE = '~~' + '~~';
+	const DECLINE_REASON_HERE = '{' + '{subst:Decline reason here}}'; // broken up to prevent processing
+	const ADVERT = ' ([[User:Enterprisey/unblock-review|unblock-review]])';
 
-    // Making this a function for unit test reasons.
-    function getInitialText(wikitext, appealReason) {
-        // https://stackoverflow.com/a/6969486/3480193
-        function escapeRegExp(string) {
-            return string.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'); // $& means the whole matched string
-        }
+	// Making this a function for unit test reasons.
+	function getInitialText( wikitext, appealReason ) {
+		// https://stackoverflow.com/a/6969486/3480193
+		function escapeRegExp( string ) {
+			return string.replace( /[.*+?^${}()|[\]\\]/g, '\\$&' ); // $& means the whole matched string
+		}
 
-        let regEx = new RegExp(escapeRegExp(appealReason), 'g');
-        let matches = wikitext.matchAll(regEx);
-        matches = [...matches];
-        if( matches.length === 0 ) {
-            throw new Error( "Searching for target text failed!" );
-        }
-        for ( let match of matches ) {
-            var textIdx = match.index;
-            var startIdx = textIdx;
+		const regEx = new RegExp( escapeRegExp( appealReason ), 'g' );
+		let matches = wikitext.matchAll( regEx );
+		matches = [ ...matches ];
+		if ( matches.length === 0 ) {
+			throw new Error( 'Searching for target text failed!' );
+		}
+		for ( const match of matches ) {
+			const textIdx = match.index;
+			let startIdx = textIdx;
 
-            // check for {{tlx|unblock. if found, this isn't what we want, skip.
-            let startOfSplice = startIdx - 50 < 0 ? 0 : startIdx - 50;
-            var chunkFiftyCharactersWide = wikitext.substring(startOfSplice, startIdx);
-            if ( /\{\{\s*tlx\s*\|\s*unblock/i.test(chunkFiftyCharactersWide) ) {
-                continue;
-            }
+			// check for {{tlx|unblock. if found, this isn't what we want, skip.
+			const startOfSplice = startIdx - 50 < 0 ? 0 : startIdx - 50;
+			const chunkFiftyCharactersWide = wikitext.substring( startOfSplice, startIdx );
+			if ( /\{\{\s*tlx\s*\|\s*unblock/i.test( chunkFiftyCharactersWide ) ) {
+				continue;
+			}
 
-            let i = 0;
-            while( wikitext[startIdx] != "{" && i < 50 ) {
-                startIdx--;
-                i++;
-            }
-            if( i == 50 ) {
-                continue;
-            }
+			let i = 0;
+			while ( wikitext[ startIdx ] != '{' && i < 50 ) {
+				startIdx--;
+				i++;
+			}
+			if ( i == 50 ) {
+				continue;
+			}
 
-            startIdx--; // templates start with two opening curly braces
+			startIdx--; // templates start with two opening curly braces
 
-            var initialText = wikitext.substring( startIdx, textIdx );
-            return initialText;
-        }
+			const initialText = wikitext.substring( startIdx, textIdx );
+			return initialText;
+		}
 
-        throw new Error( "Searching backwards failed!" );
-    }
+		throw new Error( 'Searching backwards failed!' );
+	}
 
-    if( mw.config.get( "wgNamespaceNumber" ) === 3 ) {
-        /**
-         * Is there a signature (four tildes) present in the given text,
-         * outside of a nowiki element?
-         */
-        function hasSig( text ) {
-            // no literal signature?
-            if( text.indexOf( SIGNATURE ) < 0 ) return false;
+	if ( mw.config.get( 'wgNamespaceNumber' ) === 3 ) {
+		/**
+		 * Is there a signature (four tildes) present in the given text,
+		 * outside of a nowiki element?
+		 */
+		function hasSig( text ) {
+			// no literal signature?
+			if ( text.indexOf( SIGNATURE ) < 0 ) {
+				return false;
+			}
 
-            // if there's a literal signature and no nowiki elements,
-            // there must be a real signature
-            if( text.indexOf( "<nowiki>" ) < 0 ) return true;
+			// if there's a literal signature and no nowiki elements,
+			// there must be a real signature
+			if ( text.indexOf( '<nowiki>' ) < 0 ) {
+				return true;
+			}
 
-            // Save all nowiki spans
-            var nowikiSpanStarts = []; // list of ignored span beginnings
-            var nowikiSpanLengths = []; // list of ignored span lengths
-            var NOWIKI_RE = /<nowiki>.*?<\/nowiki>/g;
-            var spanMatch;
-            do {
-                spanMatch = NOWIKI_RE.exec( text );
-                if( spanMatch ) {
-                    nowikiSpanStarts.push( spanMatch.index );
-                    nowikiSpanLengths.push( spanMatch[0].length );
-                }
-            } while( spanMatch );
+			// Save all nowiki spans
+			const nowikiSpanStarts = []; // list of ignored span beginnings
+			const nowikiSpanLengths = []; // list of ignored span lengths
+			const NOWIKI_RE = /<nowiki>.*?<\/nowiki>/g;
+			let spanMatch;
+			do {
+				spanMatch = NOWIKI_RE.exec( text );
+				if ( spanMatch ) {
+					nowikiSpanStarts.push( spanMatch.index );
+					nowikiSpanLengths.push( spanMatch[ 0 ].length );
+				}
+			} while ( spanMatch );
 
-            // So that we don't check every ignore span every time
-            var nowikiSpanStartIdx = 0;
+			// So that we don't check every ignore span every time
+			let nowikiSpanStartIdx = 0;
 
-            var SIG_RE = new RegExp( SIGNATURE, "g" );
-            var sigMatch;
+			const SIG_RE = new RegExp( SIGNATURE, 'g' );
+			let sigMatch;
 
-            matchLoop:
-            do {
-                sigMatch = SIG_RE.exec( text );
-                if( sigMatch ) {
-                    // Check that we're not inside a nowiki
-                    for( var nwIdx = nowikiSpanStartIdx; nwIdx <
+			matchLoop:
+			do {
+				sigMatch = SIG_RE.exec( text );
+				if ( sigMatch ) {
+					// Check that we're not inside a nowiki
+					for ( let nwIdx = nowikiSpanStartIdx; nwIdx <
                         nowikiSpanStarts.length; nwIdx++ ) {
-                        if( sigMatch.index > nowikiSpanStarts[nwIdx] ) {
-                            if ( sigMatch.index + sigMatch[0].length <=
-                                nowikiSpanStarts[nwIdx] + nowikiSpanLengths[nwIdx] ) {
+						if ( sigMatch.index > nowikiSpanStarts[ nwIdx ] ) {
+							if ( sigMatch.index + sigMatch[ 0 ].length <=
+                                nowikiSpanStarts[ nwIdx ] + nowikiSpanLengths[ nwIdx ] ) {
 
-                                // Invalid sig
-                                continue matchLoop;
-                            } else {
+								// Invalid sig
+								continue matchLoop;
+							} else {
 
-                                // We'll never encounter this span again, since
-                                // headers only get later and later in the wikitext
-                                nowikiSpanStartIdx = nwIdx;
-                            }
-                        }
-                    }
+								// We'll never encounter this span again, since
+								// headers only get later and later in the wikitext
+								nowikiSpanStartIdx = nwIdx;
+							}
+						}
+					}
 
-                    // We aren't inside a nowiki
-                    return true;
-                }
-            } while( sigMatch );
-            return false;
-        }
+					// We aren't inside a nowiki
+					return true;
+				}
+			} while ( sigMatch );
+			return false;
+		}
 
-        /**
-         * Given the div of an unblock request, set up the UI and event
-         * listeners.
-         */
-        function setUpUi( unblockDiv ) {
-            var container = document.createElement( "table" );
-            container.className = "unblock-review";
-            var hrEl = unblockDiv.querySelector( "hr" );
-            container.innerHTML = "<tr><td class='reason-container' rowspan='2'>" +
+		/**
+		 * Given the div of an unblock request, set up the UI and event
+		 * listeners.
+		 */
+		function setUpUi( unblockDiv ) {
+			const container = document.createElement( 'table' );
+			container.className = 'unblock-review';
+			const hrEl = unblockDiv.querySelector( 'hr' );
+			container.innerHTML = "<tr><td class='reason-container' rowspan='2'>" +
                 "<textarea class='unblock-review-reason mw-ui-input'" +
-                " placeholder='Reason for accepting/declining here'>" + DECLINE_REASON_HERE + "</textarea></td>" +
+                " placeholder='Reason for accepting/declining here'>" + DECLINE_REASON_HERE + '</textarea></td>' +
                 "<td><button class='unblock-review-accept mw-ui-button mw-ui-progressive'>Accept</button></td></tr>" +
                 "<tr><td><button class='unblock-review-decline mw-ui-button mw-ui-destructive'>Decline</button></td></tr>";
-            unblockDiv.insertBefore( container, hrEl.previousElementSibling );
-            var reasonArea = container.querySelector( "textarea" );
-            $( container ).find( "button" ).click( function () {
-                var action = $( this ).text().toLowerCase();
-                var appealReason = hrEl.nextElementSibling.nextElementSibling.childNodes[0].textContent;
-                $.getJSON(
-                    mw.util.wikiScript( "api" ),
-                    {
-                        format: "json",
-                        action: "query",
-                        prop: "revisions",
-                        rvprop: "content",
-                        rvlimit: 1,
-                        titles: mw.config.get( "wgPageName" )
-                    }
-                ).done( function ( data ) {
-                    // Extract wikitext from API response
-                    var pageId = Object.keys(data.query.pages)[0];
-                    wikitext = data.query.pages[pageId].revisions[0]["*"];
+			unblockDiv.insertBefore( container, hrEl.previousElementSibling );
+			const reasonArea = container.querySelector( 'textarea' );
+			$( container ).find( 'button' ).click( function () {
+				const action = $( this ).text().toLowerCase();
+				const appealReason = hrEl.nextElementSibling.nextElementSibling.childNodes[ 0 ].textContent;
+				$.getJSON(
+					mw.util.wikiScript( 'api' ),
+					{
+						format: 'json',
+						action: 'query',
+						prop: 'revisions',
+						rvprop: 'content',
+						rvlimit: 1,
+						titles: mw.config.get( 'wgPageName' )
+					}
+				).done( ( data ) => {
+					// Extract wikitext from API response
+					const pageId = Object.keys( data.query.pages )[ 0 ];
+					wikitext = data.query.pages[ pageId ].revisions[ 0 ][ '*' ];
 
-                    var initialText = getInitialText(wikitext, appealReason);
+					const initialText = getInitialText( wikitext, appealReason );
 
-                    // Build accept/decline reason
-                    var reason = reasonArea.value;
-                    if( !reason.trim() ) {
-                        reason = DECLINE_REASON_HERE + " " + SIGNATURE;
-                    } else if( !hasSig( reason ) ) {
-                        reason = reason + " " + SIGNATURE;
-                    }
-                    wikitext = wikitext.replace( initialText + appealReason, "{" +
-                        "{unblock reviewed|" + action + "=" + reason + "|1=" + appealReason );
+					// Build accept/decline reason
+					let reason = reasonArea.value;
+					if ( !reason.trim() ) {
+						reason = DECLINE_REASON_HERE + ' ' + SIGNATURE;
+					} else if ( !hasSig( reason ) ) {
+						reason = reason + ' ' + SIGNATURE;
+					}
+					wikitext = wikitext.replace( initialText + appealReason, '{' +
+                        '{unblock reviewed|' + action + '=' + reason + '|1=' + appealReason );
 
-                    var summary = ( action === "accept" ? "Accepting" : "Declining" ) +
-                        " unblock request" + ADVERT;
+					const summary = ( action === 'accept' ? 'Accepting' : 'Declining' ) +
+                        ' unblock request' + ADVERT;
 
-                    ( new mw.Api() ).postWithToken( "csrf", {
-                        action: "edit",
-                        title: mw.config.get( "wgPageName" ),
-                        summary: summary,
-                        text: wikitext
-                    } ).done ( function ( data ) {
-                        if ( data && data.edit && data.edit.result && data.edit.result == "Success" ) {
-                            window.location.reload( true );
-                        } else {
-                            console.log( data );
-                        }
-                    } );
-                } );
-            } );
-        }
+					( new mw.Api() ).postWithToken( 'csrf', {
+						action: 'edit',
+						title: mw.config.get( 'wgPageName' ),
+						summary: summary,
+						text: wikitext
+					} ).done( ( data ) => {
+						if ( data && data.edit && data.edit.result && data.edit.result == 'Success' ) {
+							window.location.reload( true );
+						} else {
+							console.log( data );
+						}
+					} );
+				} );
+			} );
+		}
 
-        $.when( $.ready, mw.loader.using( [ "mediawiki.api", "mediawiki.util" ] ) ).then( function () {
-            mw.util.addCSS(
-                ".unblock-review td { padding: 0 }" +
-                "td.reason-container { padding-right: 1em; width: 30em }" +
-                ".unblock-review-reason { height: 5em }" );
-            importStylesheet( "User:Enterprisey/mw-ui-button.css" );
-            importStylesheet( "User:Enterprisey/mw-ui-input.css" );
-            var userBlockBoxes = document.querySelectorAll( "div.user-block" );
-            for( var i = 0, n = userBlockBoxes.length; i < n; i++ ) {
-                if( userBlockBoxes[i].style["background-color"] !== UNBLOCK_REQ_COLOR ) {
-                    continue;
-                }
-                
-                // We now have a pending unblock request - add UI
-                setUpUi( userBlockBoxes[i] );
-            }
-        } );
-    }
-} )();
+		$.when( $.ready, mw.loader.using( [ 'mediawiki.api', 'mediawiki.util' ] ) ).then( () => {
+			mw.util.addCSS(
+				'.unblock-review td { padding: 0 }' +
+                'td.reason-container { padding-right: 1em; width: 30em }' +
+                '.unblock-review-reason { height: 5em }' );
+			importStylesheet( 'User:Enterprisey/mw-ui-button.css' );
+			importStylesheet( 'User:Enterprisey/mw-ui-input.css' );
+			const userBlockBoxes = document.querySelectorAll( 'div.user-block' );
+			for ( let i = 0, n = userBlockBoxes.length; i < n; i++ ) {
+				if ( userBlockBoxes[ i ].style[ 'background-color' ] !== UNBLOCK_REQ_COLOR ) {
+					continue;
+				}
+
+				// We now have a pending unblock request - add UI
+				setUpUi( userBlockBoxes[ i ] );
+			}
+		} );
+	}
+}() );
 // </nowiki>
