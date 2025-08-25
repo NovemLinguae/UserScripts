3aa4681c1afb9f626f64e76eadf3676cee1eb8eb
NovemLinguae <novemlinguae@gmail.com>
2025-08-25 09:41:08 -0700

    ReFillLink: create

---
+++ ReFillLink.js
@@ -0,0 +1,6 @@
+// Thanks to [[User:Qcne]]♂ for the idea. Please uninstall any old ReFill scripts to avoid double links!
+mw.util.addPortletLink(
+	'p-cactions',
+	'https://refill.toolforge.org/ng/result.php?page=' + encodeURIComponent( mw.config.get( 'wgPageName' ) ) + '&defaults=y&nowatch=y&wiki=en',
+	'reFill'
+);
