b8097ba22ab1f80ce9d48adf2e867792f6087e8d
NovemLinguae <novemlinguae@gmail.com>
2025-09-01 01:21:50 -0700

    ReFillLink: don't load on watchlist page

--- ReFillLink.js
+++ ReFillLink.js
@@ -1,6 +1,8 @@
 // Thanks to [[User:Qcne]] for the idea. Please uninstall any old ReFill scripts to avoid double links!
-mw.util.addPortletLink(
-	'p-cactions',
-	'https://refill.toolforge.org/ng/result.php?page=' + encodeURIComponent( mw.config.get( 'wgPageName' ) ) + '&defaults=y&nowatch=y&wiki=en',
-	'reFill'
-);
+if ( mw.config.get( 'wgNamespaceNumber' ) >= 0 ) {
+	mw.util.addPortletLink(
+		'p-cactions',
+		'https://refill.toolforge.org/ng/result.php?page=' + encodeURIComponent( mw.config.get( 'wgPageName' ) ) + '&defaults=y&nowatch=y&wiki=en',
+		'reFill'
+	);
+}
