291efab007ed69b4b0e75f842dd9aa42e59e121c
NovemLinguae <novemlinguae@gmail.com>
2025-09-12 23:31:45 -0700

    CiteHighlighter: fix NPPSG to array tool
    broke due to Wikimedia enforcing user agent rule

--- CiteHighlighter/deploy source list tool/index.php
+++ CiteHighlighter/deploy source list tool/index.php
@@ -6,8 +6,15 @@
 function getWikitextFromEnWiki($article) {
 	$article = rawurlencode($article);
 	$url = "https://en.wikipedia.org/w/api.php?action=query&prop=revisions&titles=$article&rvslots=*&rvprop=content&formatversion=2&format=json";
-	$jsonString = file_get_contents($url);
-	$json = json_decode($jsonString, true);
+
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL, $url);
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($ch, CURLOPT_USERAGENT, '[[w:User:Novem Linguae]]\'s "NPPSG and MySources to array tool". This is related to updating the CiteHighlighter user script.');
+	$response = curl_exec($ch);
+	curl_close($ch);
+
+	$json = json_decode($response, true);
 	return $json['query']['pages'][0]['revisions'][0]['slots']['main']['content'];
 }
 
