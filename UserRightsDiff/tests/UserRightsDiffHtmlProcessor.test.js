019ee521989b4816534321c6a4112fe2d384e0ad
NovemLinguae <novemlinguae@gmail.com>
2025-09-14 16:49:37 -0700

    UserRightsDiff: test class, not copy pasted function

--- UserRightsDiff/tests/UserRightsDiffHtmlProcessor.test.js
+++ UserRightsDiff/tests/UserRightsDiffHtmlProcessor.test.js
@@ -1,60 +1,51 @@
-/** Don't delete "(none)". Delete all other parentheses and tags. */
-function deleteParenthesesAndTags( text ) {
-	// delete unicode character U+200E. this whitespace character shows up on watchlists, and causes an extra space if not deleted.
-	text = text.replace( '\u200E', '' );
-	// get rid of 2 layers of nested parentheses. will help with some edge cases.
-	text = text.replace( /\([^()]*\([^()]*\)[^()]*\)/gs, '' );
-	// get rid of 1 layer of nested parentheses, except for (none)
-	text = text.replace( /(?!\(none\))\(.*?\){1,}/gs, '' );
-	// remove Tag: and anything after it
-	text = text.replace( / Tags?:.*?$/, '' );
-	// cleanup so it's easier to write unit tests (output doesn't have extra spaces in it)
-	text = text.replace( / {2,}/gs, ' ' );
-	text = text.replace( /(\S) ,/gs, '$1,' );
-	text = text.trim();
-	return text;
-}
+import { UserRightsDiffHtmlProcessor } from '../modules/UserRightsDiffHtmlProcessor.js';
+
+// Babel is required to use ES6 module syntax
+// Copy package.json and .babelrc from a project that already has this working
+// Babel tutorial: https://www.sitepoint.com/babel-beginners-guide/
+
+const htmlProcessor = new UserRightsDiffHtmlProcessor();
 
 describe( 'deleteParenthesesAndTags(text)', () => {
 	test( 'Basic', () => {
 		const text = '10:21, April 14, 2019 AGK talk contribs changed group membership for ArbClerkBot from confirmed user to IP block exempt (temporary, until 10:21, July 14, 2019) (+IPBE; pursuant to conditions at WP:IPEXEMPTCONDITIONS) (thank)';
 		const output = '10:21, April 14, 2019 AGK talk contribs changed group membership for ArbClerkBot from confirmed user to IP block exempt';
-		expect( deleteParenthesesAndTags( text ) ).toBe( output );
+		expect( htmlProcessor.deleteParenthesesAndTags( text ) ).toBe( output );
 	} );
 
 	test( 'Skip (none)', () => {
 		const text = '10:21, April 14, 2019 AGK talk contribs changed group membership for ArbClerkBot from (none) to IP block exempt (temporary, until 10:21, July 14, 2019) (+IPBE; pursuant to conditions at WP:IPEXEMPTCONDITIONS) (thank)';
 		const output = '10:21, April 14, 2019 AGK talk contribs changed group membership for ArbClerkBot from (none) to IP block exempt';
-		expect( deleteParenthesesAndTags( text ) ).toBe( output );
+		expect( htmlProcessor.deleteParenthesesAndTags( text ) ).toBe( output );
 	} );
 
 	test( 'Nested parentheses, touching the outside parentheses', () => {
 		const text = '10:25, April 14, 2019 AGK talk contribs changed group membership for ArbClerkBot from IP block exempt (temporary, until 10:21, July 14, 2019) and confirmed user to IP block exempt (temporary, until 10:21, July 14, 2019), confirmed user and extended confirmed user (+extendedconfirmed; pages that this bot edits (under Wikipedia:Bots/Requests for approval/ArbClerkBot)) (thank)';
 		const output = '10:25, April 14, 2019 AGK talk contribs changed group membership for ArbClerkBot from IP block exempt and confirmed user to IP block exempt, confirmed user and extended confirmed user';
-		expect( deleteParenthesesAndTags( text ) ).toBe( output );
+		expect( htmlProcessor.deleteParenthesesAndTags( text ) ).toBe( output );
 	} );
 
 	test( 'Nested parentheses, not touching the outside parentheses', () => {
 		const text = '10:25, April 14, 2019 AGK talk contribs changed group membership for ArbClerkBot from IP block exempt (temporary, until 10:21, July 14, 2019) and confirmed user to IP block exempt (temporary, until 10:21, July 14, 2019), confirmed user and extended confirmed user (+extendedconfirmed; pages that this bot edits (under Wikipedia:Bots/Requests for approval/ArbClerkBot) may from time to time have their protection levels increased) (thank)';
 		const output = '10:25, April 14, 2019 AGK talk contribs changed group membership for ArbClerkBot from IP block exempt and confirmed user to IP block exempt, confirmed user and extended confirmed user';
-		expect( deleteParenthesesAndTags( text ) ).toBe( output );
+		expect( htmlProcessor.deleteParenthesesAndTags( text ) ).toBe( output );
 	} );
 
 	test( 'Tag 1', () => {
 		const text = '10:52, December 11, 2021 Chemkatz talk contribs was automatically updated from (none) to extended confirmed user Tag: Visual edit (thank)';
 		const output = '10:52, December 11, 2021 Chemkatz talk contribs was automatically updated from (none) to extended confirmed user';
-		expect( deleteParenthesesAndTags( text ) ).toBe( output );
+		expect( htmlProcessor.deleteParenthesesAndTags( text ) ).toBe( output );
 	} );
 
 	test( 'Tag 2', () => {
 		const text = '06:22, December 11, 2021 IamSouravBiswas2000 talk contribs was automatically updated from (none) to extended confirmed user Tags: Mobile edit Mobile web edit Advanced mobile edit (thank)';
 		const output = '06:22, December 11, 2021 IamSouravBiswas2000 talk contribs was automatically updated from (none) to extended confirmed user';
-		expect( deleteParenthesesAndTags( text ) ).toBe( output );
+		expect( htmlProcessor.deleteParenthesesAndTags( text ) ).toBe( output );
 	} );
 
 	test( 'Tag 3', () => {
 		const text = '10:23, December 10, 2021 FiddleheadLady talk contribs was automatically updated from (none) to extended confirmed user Tag: 2017 wikitext editor (thank)';
 		const output = '10:23, December 10, 2021 FiddleheadLady talk contribs was automatically updated from (none) to extended confirmed user';
-		expect( deleteParenthesesAndTags( text ) ).toBe( output );
+		expect( htmlProcessor.deleteParenthesesAndTags( text ) ).toBe( output );
 	} );
 } );
