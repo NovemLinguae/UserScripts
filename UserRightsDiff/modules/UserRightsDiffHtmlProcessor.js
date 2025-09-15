3cbcc11cf4cc3e53f531787dd45524a7aa899a0e
NovemLinguae <novemlinguae@gmail.com>
2025-09-14 17:04:28 -0700

    UserRightsDiff: extract class
    this will be the last commit that works with the old log entry style. the next commit will begin rewriting this to work with the new log entry style

--- UserRightsDiff/modules/UserRightsDiffHtmlProcessor.js
+++ UserRightsDiff/modules/UserRightsDiffHtmlProcessor.js
@@ -1,4 +1,4 @@
-// import { UserRightsDiffStringProcessor } from './UserRightsDiffStringProcessor';
+import { UserRightsDiffStringProcessor } from './UserRightsDiffStringProcessor';
 
 export class UserRightsDiffHtmlProcessor {
 	constructor( $ ) {
@@ -47,9 +47,10 @@ export class UserRightsDiffHtmlProcessor {
 
 	checkLine( el ) {
 		const text = this.$( el ).text();
+		const stringProcessor = new UserRightsDiffStringProcessor();
 		let from, to;
 		try {
-			[ from, to ] = this.logEntryStringToArrays( text );
+			[ from, to ] = stringProcessor.logEntryStringToArrays( text );
 		} catch ( err ) {
 			throw new Error( 'UserRightsDiff.js error. Error was: ' + err + '. Input text was: ' + this.$( el ).text() );
 		}
@@ -65,54 +66,6 @@ export class UserRightsDiffHtmlProcessor {
 			'<span class="user-rights-diff" style="background-color:lightgray">[NO CHANGE]</span>' :
 			'';
 		this.$( el ).append( `<br />${ added } ${ removed } ${ noChange }` );
-	}
-
-	/** Returns 2 arrays. For example: [ [ 'autopatrolled' ], [ 'sysop' ] ] */
-	logEntryStringToArrays( text ) {
-		text = this.deleteParenthesesAndTags( text );
-		text = this.deleteBeginningOfLogEntry( text );
-		const matches = / from (.*?) to (.*?)(?: \(.*)?$/.exec( text );
-		const from = this.permStringToArray( matches[ 1 ] );
-		const to = this.permStringToArray( matches[ 2 ] );
-		return [ from, to ];
-	}
-
-	/** Don't delete "(none)". Delete all other parentheses and tags. */
-	deleteParenthesesAndTags( text ) {
-		// delete unicode character U+200E. this whitespace character shows up on watchlists, and causes an extra space if not deleted.
-		text = text.replace( '\u200E', '' );
-		// get rid of 2 layers of nested parentheses. will help with some edge cases.
-		text = text.replace( /\([^()]*\([^()]*\)[^()]*\)/gs, '' );
-		// get rid of 1 layer of nested parentheses, except for (none)
-		text = text.replace( /(?!\(none\))\(.*?\){1,}/gs, '' );
-		// remove Tag: and anything after it
-		text = text.replace( / Tags?:.*?$/, '' );
-		// cleanup so it's easier to write unit tests (output doesn't have extra spaces in it)
-		text = text.replace( / {2,}/gs, ' ' );
-		text = text.replace( /(\S) ,/gs, '$1,' );
-		text = text.trim();
-		return text;
-	}
-
-	/** Fixes a bug where the username contains the word "from", which is searched for by a RegEx later. */
-	deleteBeginningOfLogEntry( text ) {
-		text = text.replace( /^.*changed group membership for .* from /, ' from ' );
-		text = text.replace( /^.*was automatically updated from /gs, ' from ' );
-		return text;
-	}
-
-	permStringToArray( string ) {
-		string = string.replace( /^(.*) and (.*?$)/, '$1, $2' );
-		if ( string === '(none)' ) {
-			return [];
-		}
-		const array = string.split( ', ' ).map( ( str ) => {
-			str = str.trim();
-			// remove fragments of punctuation. can result when trying to delete nested parentheses. will delete fragments such as " .)"
-			str = str.replace( /[\s.,/#!$%^&*;:{}=\-_`~()]{2,}/g, '' );
-			return str;
-		} );
-		return array;
 	}
 
 	permArrayToString( array ) {
--- UserRightsDiff/tests/UserRightsDiffStringProcessor.test.js
+++ UserRightsDiff/tests/UserRightsDiffStringProcessor.test.js
@@ -1,51 +1,51 @@
-import { UserRightsDiffHtmlProcessor } from '../modules/UserRightsDiffHtmlProcessor.js';
+import { UserRightsDiffStringProcessor } from '../modules/UserRightsDiffStringProcessor.js';
 
 // Babel is required to use ES6 module syntax
 // Copy package.json and .babelrc from a project that already has this working
 // Babel tutorial: https://www.sitepoint.com/babel-beginners-guide/
 
-const htmlProcessor = new UserRightsDiffHtmlProcessor();
+const stringProcessor = new UserRightsDiffStringProcessor();
 
 describe( 'deleteParenthesesAndTags(text)', () => {
 	test( 'Basic', () => {
 		const text = '10:21, April 14, 2019 AGK talk contribs changed group membership for ArbClerkBot from confirmed user to IP block exempt (temporary, until 10:21, July 14, 2019) (+IPBE; pursuant to conditions at WP:IPEXEMPTCONDITIONS) (thank)';
 		const output = '10:21, April 14, 2019 AGK talk contribs changed group membership for ArbClerkBot from confirmed user to IP block exempt';
-		expect( htmlProcessor.deleteParenthesesAndTags( text ) ).toBe( output );
+		expect( stringProcessor.deleteParenthesesAndTags( text ) ).toBe( output );
 	} );
 
 	test( 'Skip (none)', () => {
 		const text = '10:21, April 14, 2019 AGK talk contribs changed group membership for ArbClerkBot from (none) to IP block exempt (temporary, until 10:21, July 14, 2019) (+IPBE; pursuant to conditions at WP:IPEXEMPTCONDITIONS) (thank)';
 		const output = '10:21, April 14, 2019 AGK talk contribs changed group membership for ArbClerkBot from (none) to IP block exempt';
-		expect( htmlProcessor.deleteParenthesesAndTags( text ) ).toBe( output );
+		expect( stringProcessor.deleteParenthesesAndTags( text ) ).toBe( output );
 	} );
 
 	test( 'Nested parentheses, touching the outside parentheses', () => {
 		const text = '10:25, April 14, 2019 AGK talk contribs changed group membership for ArbClerkBot from IP block exempt (temporary, until 10:21, July 14, 2019) and confirmed user to IP block exempt (temporary, until 10:21, July 14, 2019), confirmed user and extended confirmed user (+extendedconfirmed; pages that this bot edits (under Wikipedia:Bots/Requests for approval/ArbClerkBot)) (thank)';
 		const output = '10:25, April 14, 2019 AGK talk contribs changed group membership for ArbClerkBot from IP block exempt and confirmed user to IP block exempt, confirmed user and extended confirmed user';
-		expect( htmlProcessor.deleteParenthesesAndTags( text ) ).toBe( output );
+		expect( stringProcessor.deleteParenthesesAndTags( text ) ).toBe( output );
 	} );
 
 	test( 'Nested parentheses, not touching the outside parentheses', () => {
 		const text = '10:25, April 14, 2019 AGK talk contribs changed group membership for ArbClerkBot from IP block exempt (temporary, until 10:21, July 14, 2019) and confirmed user to IP block exempt (temporary, until 10:21, July 14, 2019), confirmed user and extended confirmed user (+extendedconfirmed; pages that this bot edits (under Wikipedia:Bots/Requests for approval/ArbClerkBot) may from time to time have their protection levels increased) (thank)';
 		const output = '10:25, April 14, 2019 AGK talk contribs changed group membership for ArbClerkBot from IP block exempt and confirmed user to IP block exempt, confirmed user and extended confirmed user';
-		expect( htmlProcessor.deleteParenthesesAndTags( text ) ).toBe( output );
+		expect( stringProcessor.deleteParenthesesAndTags( text ) ).toBe( output );
 	} );
 
 	test( 'Tag 1', () => {
 		const text = '10:52, December 11, 2021 Chemkatz talk contribs was automatically updated from (none) to extended confirmed user Tag: Visual edit (thank)';
 		const output = '10:52, December 11, 2021 Chemkatz talk contribs was automatically updated from (none) to extended confirmed user';
-		expect( htmlProcessor.deleteParenthesesAndTags( text ) ).toBe( output );
+		expect( stringProcessor.deleteParenthesesAndTags( text ) ).toBe( output );
 	} );
 
 	test( 'Tag 2', () => {
 		const text = '06:22, December 11, 2021 IamSouravBiswas2000 talk contribs was automatically updated from (none) to extended confirmed user Tags: Mobile edit Mobile web edit Advanced mobile edit (thank)';
 		const output = '06:22, December 11, 2021 IamSouravBiswas2000 talk contribs was automatically updated from (none) to extended confirmed user';
-		expect( htmlProcessor.deleteParenthesesAndTags( text ) ).toBe( output );
+		expect( stringProcessor.deleteParenthesesAndTags( text ) ).toBe( output );
 	} );
 
 	test( 'Tag 3', () => {
 		const text = '10:23, December 10, 2021 FiddleheadLady talk contribs was automatically updated from (none) to extended confirmed user Tag: 2017 wikitext editor (thank)';
 		const output = '10:23, December 10, 2021 FiddleheadLady talk contribs was automatically updated from (none) to extended confirmed user';
-		expect( htmlProcessor.deleteParenthesesAndTags( text ) ).toBe( output );
+		expect( stringProcessor.deleteParenthesesAndTags( text ) ).toBe( output );
 	} );
 } );
