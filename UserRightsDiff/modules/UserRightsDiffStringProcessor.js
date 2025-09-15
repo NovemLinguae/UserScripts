86294efc34e52d16e5c038f55c29aec7640bc353
NovemLinguae <novemlinguae@gmail.com>
2025-09-14 17:44:46 -0700

    UserRightsDiff: fix #244 support new wordings "granted" and "kept"

--- UserRightsDiff/modules/UserRightsDiffStringProcessor.js
+++ UserRightsDiff/modules/UserRightsDiffStringProcessor.js
@@ -2,49 +2,40 @@ export class UserRightsDiffStringProcessor {
 
 	/** Returns 2 arrays. For example: [ [ 'autopatrolled' ], [ 'sysop' ] ] */
 	logEntryStringToArrays( text ) {
-		text = this.deleteParenthesesAndTags( text );
-		text = this.deleteBeginningOfLogEntry( text );
-		const matches = / from (.*?) to (.*?)(?: \(.*)?$/.exec( text );
-		const from = this.permStringToArray( matches[ 1 ] );
-		const to = this.permStringToArray( matches[ 2 ] );
+		const fromMatches = /revoked ([^;(]+)/.exec( text );
+		const fromMatch = fromMatches && fromMatches[ 1 ];
+		const from = this.permStringToArray( fromMatch );
+
+		const toMatches = /granted ([^;(]+)/.exec( text );
+		const toMatch = toMatches && toMatches[ 1 ];
+		let to = this.permStringToArray( toMatch );
+
+		const autoUpdatedMatches = /automatically updated .+ to ([a-z ]+)/.exec( text );
+		const autoUpdateMatch = autoUpdatedMatches && autoUpdatedMatches[ 1 ];
+		const autoUpdate = this.permStringToArray( autoUpdateMatch );
+		// array_merge( to, autoUpdate )
+		to = to.concat( autoUpdate.filter( ( x ) => !to.includes( x ) ) );
+
 		return [ from, to ];
 	}
 
-	/** Don't delete "(none)". Delete all other parentheses and tags. */
-	deleteParenthesesAndTags( text ) {
-		// delete unicode character U+200E. this whitespace character shows up on watchlists, and causes an extra space if not deleted.
-		text = text.replace( '\u200E', '' );
-		// get rid of 2 layers of nested parentheses. will help with some edge cases.
-		text = text.replace( /\([^()]*\([^()]*\)[^()]*\)/gs, '' );
-		// get rid of 1 layer of nested parentheses, except for (none)
-		text = text.replace( /(?!\(none\))\(.*?\){1,}/gs, '' );
-		// remove Tag: and anything after it
-		text = text.replace( / Tags?:.*?$/, '' );
-		// cleanup so it's easier to write unit tests (output doesn't have extra spaces in it)
-		text = text.replace( / {2,}/gs, ' ' );
-		text = text.replace( /(\S) ,/gs, '$1,' );
-		text = text.trim();
-		return text;
-	}
-
-	/** Fixes a bug where the username contains the word "from", which is searched for by a RegEx later. */
-	deleteBeginningOfLogEntry( text ) {
-		text = text.replace( /^.*changed group membership for .* from /, ' from ' );
-		text = text.replace( /^.*was automatically updated from /gs, ' from ' );
-		return text;
-	}
-
 	permStringToArray( string ) {
+		if ( string === null ) {
+			return [];
+		}
+
 		string = string.replace( /^(.*) and (.*?$)/, '$1, $2' );
 		if ( string === '(none)' ) {
 			return [];
 		}
+
 		const array = string.split( ', ' ).map( ( str ) => {
 			str = str.trim();
 			// remove fragments of punctuation. can result when trying to delete nested parentheses. will delete fragments such as " .)"
 			str = str.replace( /[\s.,/#!$%^&*;:{}=\-_`~()]{2,}/g, '' );
 			return str;
 		} );
+
 		return array;
 	}
 }
--- UserRightsDiff/tests/UserRightsDiffStringProcessor.test.js
+++ UserRightsDiff/tests/UserRightsDiffStringProcessor.test.js
@@ -6,46 +6,60 @@ import { UserRightsDiffStringProcessor } from '../modules/UserRightsDiffStringPr
 
 const stringProcessor = new UserRightsDiffStringProcessor();
 
-describe( 'deleteParenthesesAndTags(text)', () => {
-	test( 'Basic', () => {
-		const text = '10:21, April 14, 2019 AGK talk contribs changed group membership for ArbClerkBot from confirmed user to IP block exempt (temporary, until 10:21, July 14, 2019) (+IPBE; pursuant to conditions at WP:IPEXEMPTCONDITIONS) (thank)';
-		const output = '10:21, April 14, 2019 AGK talk contribs changed group membership for ArbClerkBot from confirmed user to IP block exempt';
-		expect( stringProcessor.deleteParenthesesAndTags( text ) ).toBe( output );
+describe( 'logEntryStringToArrays( text )', () => {
+	test( 'Add one', () => {
+		const text = '(change visibility) 16:29, June 10, 2025 Novem Linguae talk contribs block changed group membership for Novem Linguae: granted election clerk; kept edit filter manager, interface administrator and administrator unchanged (+electionclerk. one of the election clerks for the upcoming 2025 administrator election)';
+		const output = [ [], [ 'election clerk' ] ];
+		expect( stringProcessor.logEntryStringToArrays( text ) ).toStrictEqual( output );
 	} );
 
-	test( 'Skip (none)', () => {
-		const text = '10:21, April 14, 2019 AGK talk contribs changed group membership for ArbClerkBot from (none) to IP block exempt (temporary, until 10:21, July 14, 2019) (+IPBE; pursuant to conditions at WP:IPEXEMPTCONDITIONS) (thank)';
-		const output = '10:21, April 14, 2019 AGK talk contribs changed group membership for ArbClerkBot from (none) to IP block exempt';
-		expect( stringProcessor.deleteParenthesesAndTags( text ) ).toBe( output );
+	test( 'Remove four', () => {
+		const text = '(change visibility) 13:32, June 17, 2023 Maxim talk contribs block changed group membership for Novem Linguae: revoked extended confirmed user, new page reviewer, pending changes reviewer and rollbacker (Wikipedia:Requests for adminship/Novem Linguae) (thank)';
+		const output = [ [ 'extended confirmed user', 'new page reviewer', 'pending changes reviewer', 'rollbacker' ], [] ];
+		expect( stringProcessor.logEntryStringToArrays( text ) ).toStrictEqual( output );
 	} );
 
-	test( 'Nested parentheses, touching the outside parentheses', () => {
-		const text = '10:25, April 14, 2019 AGK talk contribs changed group membership for ArbClerkBot from IP block exempt (temporary, until 10:21, July 14, 2019) and confirmed user to IP block exempt (temporary, until 10:21, July 14, 2019), confirmed user and extended confirmed user (+extendedconfirmed; pages that this bot edits (under Wikipedia:Bots/Requests for approval/ArbClerkBot)) (thank)';
-		const output = '10:25, April 14, 2019 AGK talk contribs changed group membership for ArbClerkBot from IP block exempt and confirmed user to IP block exempt, confirmed user and extended confirmed user';
-		expect( stringProcessor.deleteParenthesesAndTags( text ) ).toBe( output );
+	test( 'No change & temporary perms', () => {
+		const text = '(change visibility) 06:58, May 13, 2019 Xaosflux talk contribs block changed group membership for ArbClerkBot: changed expiration of IP block exempt (permanent, was: until 10:21, July 14, 2019); kept extended confirmed user unchanged (+IPBE indefintie, see Wikipedia:Bots/Requests for approval/ArbClerkBot) (thank)';
+		const output = [ [], [] ];
+		expect( stringProcessor.logEntryStringToArrays( text ) ).toStrictEqual( output );
 	} );
 
-	test( 'Nested parentheses, not touching the outside parentheses', () => {
-		const text = '10:25, April 14, 2019 AGK talk contribs changed group membership for ArbClerkBot from IP block exempt (temporary, until 10:21, July 14, 2019) and confirmed user to IP block exempt (temporary, until 10:21, July 14, 2019), confirmed user and extended confirmed user (+extendedconfirmed; pages that this bot edits (under Wikipedia:Bots/Requests for approval/ArbClerkBot) may from time to time have their protection levels increased) (thank)';
-		const output = '10:25, April 14, 2019 AGK talk contribs changed group membership for ArbClerkBot from IP block exempt and confirmed user to IP block exempt, confirmed user and extended confirmed user';
-		expect( stringProcessor.deleteParenthesesAndTags( text ) ).toBe( output );
+	test( 'Add 1, remove 6', () => {
+		const text = '(change visibility) 14:06, July 31, 2025 Primefac talk contribs block changed group membership for KylieTastic: granted administrator; revoked extended confirmed user, page mover, file mover, new page reviewer, pending changes reviewer and rollbacker; kept autopatrolled unchanged (+sysop per AELECT, Special:Permalink/1303582232) (thank)';
+		const output = [ [ 'extended confirmed user', 'page mover', 'file mover', 'new page reviewer', 'pending changes reviewer', 'rollbacker' ], [ 'administrator' ] ];
+		expect( stringProcessor.logEntryStringToArrays( text ) ).toStrictEqual( output );
 	} );
 
-	test( 'Tag 1', () => {
-		const text = '10:52, December 11, 2021 Chemkatz talk contribs was automatically updated from (none) to extended confirmed user Tag: Visual edit (thank)';
-		const output = '10:52, December 11, 2021 Chemkatz talk contribs was automatically updated from (none) to extended confirmed user';
-		expect( stringProcessor.deleteParenthesesAndTags( text ) ).toBe( output );
+	test( 'Add 2 to a user with no previous perms', () => {
+		const text = '(change visibility) 07:59, July 12, 2014 Acalamari talk contribs block changed group membership for KylieTastic: granted pending changes reviewer and rollbacker (User will make good use of these tools and can be trusted with them) (thank)';
+		const output = [ [], [ 'pending changes reviewer', 'rollbacker' ] ];
+		expect( stringProcessor.logEntryStringToArrays( text ) ).toStrictEqual( output );
 	} );
 
-	test( 'Tag 2', () => {
-		const text = '06:22, December 11, 2021 IamSouravBiswas2000 talk contribs was automatically updated from (none) to extended confirmed user Tags: Mobile edit Mobile web edit Advanced mobile edit (thank)';
-		const output = '06:22, December 11, 2021 IamSouravBiswas2000 talk contribs was automatically updated from (none) to extended confirmed user';
-		expect( stringProcessor.deleteParenthesesAndTags( text ) ).toBe( output );
+	test( 'Automatically updated', () => {
+		const text = '(change visibility) 10:52, December 11, 2021 Chemkatz talk contribs block was automatically updated from (none) to extended confirmed user';
+		const output = [ [], [ 'extended confirmed user' ] ];
+		expect( stringProcessor.logEntryStringToArrays( text ) ).toStrictEqual( output );
 	} );
 
-	test( 'Tag 3', () => {
-		const text = '10:23, December 10, 2021 FiddleheadLady talk contribs was automatically updated from (none) to extended confirmed user Tag: 2017 wikitext editor (thank)';
-		const output = '10:23, December 10, 2021 FiddleheadLady talk contribs was automatically updated from (none) to extended confirmed user';
-		expect( stringProcessor.deleteParenthesesAndTags( text ) ).toBe( output );
+	test( 'Automatically updated & tag', () => {
+		const text = '(change visibility) 10:52, December 11, 2021 Chemkatz talk contribs block was automatically updated from (none) to extended confirmed user Tag: Visual edit';
+		const output = [ [], [ 'extended confirmed user' ] ];
+		expect( stringProcessor.logEntryStringToArrays( text ) ).toStrictEqual( output );
+	} );
+} );
+
+describe( 'permStringToArray( match )', () => {
+	test( 'Null', () => {
+		const match = null;
+		const output = [];
+		expect( stringProcessor.permStringToArray( match ) ).toStrictEqual( output );
+	} );
+
+	test( 'Four', () => {
+		const match = 'extended confirmed user, new page reviewer, pending changes reviewer and rollbacker';
+		const output = [ 'extended confirmed user', 'new page reviewer', 'pending changes reviewer', 'rollbacker' ];
+		expect( stringProcessor.permStringToArray( match ) ).toStrictEqual( output );
 	} );
 } );
