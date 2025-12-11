a7c26415f7ca9aa6b88feaf994a612b002b69642
NovemLinguae <novemlinguae@gmail.com>
2025-12-11 03:07:34 -0800

    ReFillLink: fix linter error

--- ReFillLink.js
+++ ReFillLink.js
@@ -1,5 +1,5 @@
 // Thanks to [[User:Qcne]] for the idea. Please uninstall any old ReFill scripts to avoid double links!
-const pageExists = mw.config.get('wgArticleId') > 0;
+const pageExists = mw.config.get( 'wgArticleId' ) > 0;
 const notSpecialOrFileNamespace = mw.config.get( 'wgNamespaceNumber' ) >= 0;
 if ( pageExists && notSpecialOrFileNamespace ) {
 	mw.util.addPortletLink(
