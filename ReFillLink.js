c284d9cb3e380b1784ec486f1d1a6a76aca9664b
NovemLinguae <novemlinguae@gmail.com>
2025-12-11 00:12:31 -0800

    ReFillLink: don't load on non-created pages

--- ReFillLink.js
+++ ReFillLink.js
@@ -1,5 +1,7 @@
 // Thanks to [[User:Qcne]] for the idea. Please uninstall any old ReFill scripts to avoid double links!
-if ( mw.config.get( 'wgNamespaceNumber' ) >= 0 ) {
+const pageExists = mw.config.get('wgArticleId') > 0;
+const notSpecialOrFileNamespace = mw.config.get( 'wgNamespaceNumber' ) >= 0;
+if ( pageExists && notSpecialOrFileNamespace ) {
 	mw.util.addPortletLink(
 		'p-cactions',
 		'https://refill.toolforge.org/ng/result.php?page=' + encodeURIComponent( mw.config.get( 'wgPageName' ) ) + '&defaults=y&nowatch=y&wiki=en',
